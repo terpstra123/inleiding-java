ca09e7585f33a8009b4de297dc4443ecf9399f45
bjorn terpstra <bjornterpstra@hotmail.com>
2017-11-23 11:28:35 +0100

    Opzetten Show

---
+++ src/h01/Show.java
@@ -0,0 +1,18 @@
+package h01;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Show extends Applet {
+
+    //een lege method
+    public void init(){
+
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        g.
+    }
+}
